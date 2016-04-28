678dcd67c08420042feaf35b48b04a8e991dc0fc
al_hardy@live.com.au <al_hardy@live.com.au>
2016-04-28 14:09:55 +1000

    added build scripts

---
+++ test/AspNet.Metrics.Facts/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNet.Metrics.Facts
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1
+    {
+
+        [Fact]
+        public void UniTest1()
+        {
+            Assert.True(true);
+        }
+    }
+}
