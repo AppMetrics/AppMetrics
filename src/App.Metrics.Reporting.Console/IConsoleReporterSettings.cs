aaedb9ead10b4c234af23f07706385e506a2a9aa
Allan hardy <al_hardy@live.com.au>
2016-11-01 21:52:16 +1100

    license headers

--- src/App.Metrics.Reporting.Console/IConsoleReporterSettings.cs
+++ src/App.Metrics.Reporting.Console/IConsoleReporterSettings.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Allan hardy. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 namespace App.Metrics.Reporting
 {
     public interface IConsoleReporterSettings : IReporterSettings
