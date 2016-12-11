eb5990c1fca99a0f97e1f4787be27a4202c936de
Allan hardy <al_hardy@live.com.au>
2016-12-12 07:02:08 +1100

    adding license headers to influxdb reporter files

--- src/App.Metrics.Extensions.Reporting.InfluxDB/Client/LineProtocolSyntax.cs
+++ src/App.Metrics.Extensions.Reporting.InfluxDB/Client/LineProtocolSyntax.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Allan hardy. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -24,7 +28,6 @@ namespace App.Metrics.Extensions.Reporting.InfluxDB.Client
             { typeof(bool), FormatBoolean },
             { typeof(TimeSpan), FormatTimespan }
         };
-
 
         public static string EscapeName(string nameOrKey)
         {
--- src/App.Metrics.Extensions.Reporting.InfluxDB/Client/LineProtocolWriteResult.cs
+++ src/App.Metrics.Extensions.Reporting.InfluxDB/Client/LineProtocolWriteResult.cs
@@ -1,4 +1,7 @@
-﻿namespace App.Metrics.Extensions.Reporting.InfluxDB.Client
+﻿// Copyright (c) Allan hardy. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace App.Metrics.Extensions.Reporting.InfluxDB.Client
 {
     public struct LineProtocolWriteResult
     {
