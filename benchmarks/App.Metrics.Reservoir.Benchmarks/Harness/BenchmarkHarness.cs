46d40b5505c53d8878e9e9a6750ddbbd4ff62fc2
al_hardy@live.com.au <al_hardy@live.com.au>
2017-01-06 23:47:59 +1100

    upgrading all test and sample projects to dotnetcore 1.1

--- benchmarks/App.Metrics.Reservoir.Benchmarks/Harness/BenchmarkHarness.cs
+++ benchmarks/App.Metrics.Reservoir.Benchmarks/Harness/BenchmarkHarness.cs
@@ -6,7 +6,7 @@ namespace App.Metrics.Reservoir.Benchmarks.Harness
     public class BenchmarkHarness
     {
         [Fact, Trait("MediumRun", "ReservoirSamplingCompare")]
-        public void AtomicLongCompareBenchmark()
+        public void ReservoirSamplingBenchmark()
         {
             BenchmarkRunner.Run<ReservoirSamplingBenchmarks>();
         }
