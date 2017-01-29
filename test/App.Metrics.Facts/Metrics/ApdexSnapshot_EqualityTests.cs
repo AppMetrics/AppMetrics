dbbca1875b75b0e2bd998909f0a42b4c20c1700b
al_hardy@live.com.au <al_hardy@live.com.au>
2017-01-29 18:02:55 +1100

    apdex snapshot equality tests

---
+++ test/App.Metrics.Facts/Metrics/ApdexSnapshot_EqualityTests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Allan Hardy. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using App.Metrics.Apdex;
+using FluentAssertions;
+using Xunit;
+
+namespace App.Metrics.Facts.Metrics
+{
+    public class ApdexSnapshot_EqualityTests
+    {
+        [Fact]
+        public void equality_with_equals_false_when_not_same()
+        {
+            var env = new ApdexSnapshot(new[] { 1L, 2L, 3L }, 0.5);
+            var other = new ApdexSnapshot(new[] { 1L, 2L }, 0.5);
+
+            env.Equals(other).Should().Be(false);
+        }
+
+        [Fact]
+        public void equality_with_equals_operator()
+        {
+            var env = new ApdexSnapshot(new[] { 1L, 2L, 3L }, 0.5);
+            var other = new ApdexSnapshot(new[] { 1L, 2L, 3L }, 0.5);
+
+            env.Equals(other).Should().Be(true);
+        }
+
+        [Fact]
+        public void equality_with_equals_true_when_same()
+        {
+            var env = new ApdexSnapshot(new[] { 1L, 2L, 3L }, 0.5);
+            var other = new ApdexSnapshot(new[] { 1L, 2L, 3L }, 0.5);
+
+            env.Equals(other).Should().Be(true);
+        }
+
+        [Fact]
+        public void equality_with_not_equals_operator()
+        {
+            var env = new ApdexSnapshot(new[] { 1L, 2L, 3L }, 0.5);
+            var other = new ApdexSnapshot(new[] { 1L, 2L }, 0.5);
+
+            env.Equals(other).Should().Be(false);
+        }
+    }
+}
