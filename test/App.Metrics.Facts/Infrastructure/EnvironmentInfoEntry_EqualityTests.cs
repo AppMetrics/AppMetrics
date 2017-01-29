6796f3826550a6d66cbec792c9aebe81fc565991
al_hardy@live.com.au <al_hardy@live.com.au>
2017-01-29 17:53:57 +1100

    env info equality tests

---
+++ test/App.Metrics.Facts/Infrastructure/EnvironmentInfoEntry_EqualityTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Allan Hardy. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using App.Metrics.Infrastructure;
+using FluentAssertions;
+using Xunit;
+
+namespace App.Metrics.Facts.Infrastructure
+{
+    public class EnvironmentInfoEntry_EqualityTests
+    {
+        [Theory]
+        [InlineData("name", "value", true)]
+        [InlineData("name1", "value", false)]
+        [InlineData("name", "value1", false)]
+        public void equality_with_equals(string name, string value, bool expected)
+        {
+            var env = new EnvironmentInfoEntry("name", "value");
+            var other = new EnvironmentInfoEntry(name, value);
+
+            env.Equals(other).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("name", "value", true)]
+        [InlineData("name1", "value", false)]
+        [InlineData("name", "value1", false)]
+        public void equality_with_equals_operator(string name, string value, bool expected)
+        {
+            var env = new EnvironmentInfoEntry("name", "value");
+            var other = new EnvironmentInfoEntry(name, value);
+
+            (env == other).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("name", "value", true)]
+        [InlineData("name1", "value", false)]
+        [InlineData("name", "value1", false)]
+        public void equality_with_not_equals_operator(string name, string value, bool expected)
+        {
+            var env = new EnvironmentInfoEntry("name", "value");
+            var other = new EnvironmentInfoEntry(name, value);
+
+            (env != other).Should().Be(!expected);
+        }
+
+    }
+}
