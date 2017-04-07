36c461a30786cb45433db89382c17575875d50d7
al_hardy@live.com.au <al_hardy@live.com.au>
2017-04-07 22:24:22 +1000

    #106 Allowing custom formatting of metrics and health endpoint responses

---
+++ src/App.Metrics.Extensions.Middleware/Internal/NoOpHealthStatusResponseWriter.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="NoOpHealthStatusResponseWriter.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System.Threading;
+using System.Threading.Tasks;
+using App.Metrics.Extensions.Middleware.Abstractions;
+using App.Metrics.Health;
+using Microsoft.AspNetCore.Http;
+
+namespace App.Metrics.Extensions.Middleware.Internal
+{
+    internal class NoOpHealthStatusResponseWriter : IHealthResponseWriter
+    {
+        /// <inheritdoc />
+        public string ContentType => "text/plain";
+
+        /// <inheritdoc />
+        public Task WriteAsync(HttpContext context, HealthStatus healthStatus, CancellationToken token = default(CancellationToken))
+        {
+            return context.Response.WriteAsync("No formatter has been registered. See App.Metrics.Formatters.Json for example.", token);
+        }
+    }
+}
