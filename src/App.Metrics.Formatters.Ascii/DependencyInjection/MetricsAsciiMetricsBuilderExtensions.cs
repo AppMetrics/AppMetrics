49d756484ae8e1e7b1a02dc013c74103c75918bd
alhardy <al_hardy@live.com.au>
2017-08-04 22:59:23 +1000

    adding extension methods for modifying ascii and json options

---
+++ src/App.Metrics.Formatters.Ascii/DependencyInjection/MetricsAsciiMetricsBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="MetricsAsciiMetricsBuilderExtensions.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System;
+using App.Metrics.Formatters.Ascii;
+
+// ReSharper disable CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+    // ReSharper restore CheckNamespace
+{
+    public static class MetricsAsciiMetricsBuilderExtensions
+    {
+        public static IMetricsBuilder AddAsciiOptions(
+            this IMetricsBuilder builder,
+            Action<MetricsAsciiOptions> setupAction)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (setupAction == null)
+            {
+                throw new ArgumentNullException(nameof(setupAction));
+            }
+
+            builder.Services.Configure(setupAction);
+
+            return builder;
+        }
+    }
+}
---
+++ src/App.Metrics.Formatters.Json/DependencyInjection/MetricsJsonMetricsBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="MetricsJsonMetricsBuilderExtensions.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System;
+using App.Metrics.Formatters.Json;
+
+// ReSharper disable CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+    // ReSharper restore CheckNamespace
+{
+    public static class MetricsJsonMetricsBuilderExtensions
+    {
+        public static IMetricsBuilder AddJsonOptions(
+            this IMetricsBuilder builder,
+            Action<MetricsJsonOptions> setupAction)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (setupAction == null)
+            {
+                throw new ArgumentNullException(nameof(setupAction));
+            }
+
+            builder.Services.Configure(setupAction);
+
+            return builder;
+        }
+    }
+}
