91b161832b98ed4cb8c28dd2d7e584aa96c3d46e
alhardy <al_hardy@live.com.au>
2017-11-02 22:08:51 +1100

    adding metric formatter collection tests

---
+++ test/App.Metrics.Facts/Formatters/MetricsFormatterReadonlyCollectionExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="MetricsFormatterReadonlyCollectionExtensionsTests.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System.Collections.ObjectModel;
+using App.Metrics.Formatters;
+using App.Metrics.Formatters.Ascii;
+using App.Metrics.Formatters.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace App.Metrics.Facts.Formatters
+{
+    public class MetricsFormatterReadonlyCollectionExtensionsTests
+    {
+        [Fact]
+        public void Can_get_formatter_by_type()
+        {
+            // Arrange
+            var formatters = new MetricsFormatterCollection();
+            formatters.TryAdd(new MetricsJsonOutputFormatter());
+            formatters.TryAdd(new MetricsTextOutputFormatter());
+            var readonlyFormatters = new ReadOnlyCollection<IMetricsOutputFormatter>(formatters);
+
+            // Act
+            var result = readonlyFormatters.GetType<MetricsTextOutputFormatter>();
+
+            // Assert
+            result.Should().BeOfType(typeof(MetricsTextOutputFormatter));
+        }
+    }
+}
