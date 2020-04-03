22bfb08ca27b83f85ca45c32cffb99638bf9046f
Allan <al_hardy@live.com.au>
2020-04-04 08:40:54 +1100

    #536 adding datadog sandbox and http reporter

---
+++ src/Reporting/sandbox/MetricsDatadogSandboxMvc/JustForTesting/FileController.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="FileController.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
+// </copyright>
+
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsDatadogSandboxMvc.JustForTesting
+{
+    [Route("api/[controller]")]
+    public class FileController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post(IFormFile file)
+        {
+            return Created(new Uri("http://localhost"), 0);
+        }
+
+        [HttpPut]
+        public IActionResult Put(IFormFile file)
+        {
+            return Ok();
+        }
+    }
+}
