1da919d4c83d7f0211d8f9415ac004d05318f362
Allan Hardy <al_hardy@live.com.au>
2020-06-19 21:52:55 +1000

    push to github packages

--- build.ps1
+++ build.ps1
@@ -3,25 +3,25 @@ $ErrorActionPreference = "Stop";
 New-Item -ItemType Directory -Force -Path ./nuget
 
 pushd ./src/Core
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/Extensions
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/AspNetCore
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/Reporting
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/NServiceBus
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/MetaPackages
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
