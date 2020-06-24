eaccf2f56312d343a6ca9f94ee15cb9dca6c6434
Allan Hardy <al_hardy@live.com.au>
2020-06-24 21:08:08 +1000

    #570 adding sample configuring via IApplicationBuilder

---
+++ src/AspNetCore/sandbox/MetricsSandboxCoreConfigServices/Program.cs
@@ -0,0 +1,22 @@
+using App.Metrics;
+using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MetricsSandboxCoreConfigServices
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
