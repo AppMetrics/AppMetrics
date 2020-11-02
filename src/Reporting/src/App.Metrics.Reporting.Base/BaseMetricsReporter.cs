057bbf199fa1a0be2de9630cae80721b7384dc09
Maksim Lebedev <alessarik@gmail.com>
2020-11-02 20:41:49 +0300

    Adding App.Metrics.Reporting.Base project

---
+++ src/Reporting/src/App.Metrics.Reporting.Base/BaseMetricsReporter.cs
@@ -0,0 +1,88 @@
+﻿// <copyright file="BaseMetricsReporter.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
+// </copyright>
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using App.Metrics.Filters;
+using App.Metrics.Formatters;
+using App.Metrics.Formatters.Ascii;
+using App.Metrics.Logging;
+
+namespace App.Metrics.Reporting.Base
+{
+    public abstract class BaseMetricsReporter : IReportMetrics
+    {
+        private static readonly ILog Logger = LogProvider.For<BaseMetricsReporter>();
+        private readonly IMetricsOutputFormatter _defaultMetricsOutputFormatter = new MetricsTextOutputFormatter();
+
+        // ReSharper disable UnusedMember.Global
+        public BaseMetricsReporter()
+            // ReSharper restore UnusedMember.Global
+        {
+            FlushInterval = AppMetricsConstants.Reporting.DefaultFlushInterval;
+            Formatter = _defaultMetricsOutputFormatter;
+
+            Logger.Info($"Using Metrics Reporter {this}. FlushInterval: {FlushInterval}. Formatter: {Formatter}");
+        }
+
+        public BaseMetricsReporter(MetricsReportingBaseOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (options.FlushInterval < TimeSpan.Zero)
+            {
+                throw new InvalidOperationException($"{nameof(MetricsReportingBaseOptions.FlushInterval)} must not be less than zero");
+            }
+
+            Formatter = options.MetricsOutputFormatter ?? _defaultMetricsOutputFormatter;
+
+            FlushInterval = options.FlushInterval > TimeSpan.Zero
+                ? options.FlushInterval
+                : AppMetricsConstants.Reporting.DefaultFlushInterval;
+
+            Filter = options.Filter;
+
+            Logger.Info($"Using Metrics Reporter {this}. FlushInterval: {FlushInterval}. Formatter: {Formatter}");
+        }
+
+        /// <inheritdoc />
+        public IFilterMetrics Filter { get; set; }
+
+        /// <inheritdoc />
+        public TimeSpan FlushInterval { get; set; }
+
+        /// <inheritdoc />
+        public IMetricsOutputFormatter Formatter { get; set; }
+
+        /// <inheritdoc />
+        public async Task<bool> FlushAsync(MetricsDataValueSource metricsData, CancellationToken cancellationToken = default)
+        {
+            var result = false;
+
+            Logger.Trace("Flushing metrics snapshot");
+
+            using (var stream = new MemoryStream())
+            {
+                await Formatter.WriteAsync(stream, metricsData, cancellationToken);
+
+                result = await this.FlushImplAsync(stream, cancellationToken);
+            }
+
+            Logger.Trace("Flushed metrics snapshot");
+
+            return result;
+        }
+
+        public virtual async Task<bool> FlushImplAsync(MemoryStream stream, CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            throw new InvalidOperationException($"{nameof(BaseMetricsReporter.FlushImplAsync)} should be overriden");
+        }
+    }
+}
---
+++ src/Reporting/src/App.Metrics.Reporting.Base/MetricsReportingBaseOptions.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="MetricsReportingBaseOptions.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
+// </copyright>
+
+using System;
+using App.Metrics.Filters;
+using App.Metrics.Formatters;
+
+namespace App.Metrics.Reporting.Base
+{
+    /// <summary>
+    ///     Provides programmatic configuration of Base Reporting in the App Metrics framework.
+    /// </summary>
+    public class MetricsReportingBaseOptions
+    {
+        /// <summary>
+        ///     Gets or sets the <see cref="IFilterMetrics" /> to use for just this reporter.
+        /// </summary>
+        /// <value>
+        ///     The <see cref="IFilterMetrics" /> to use for this reporter.
+        /// </value>
+        public IFilterMetrics Filter { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the <see cref="IMetricsOutputFormatter" /> used to write metrics.
+        /// </summary>
+        /// <value>
+        ///     The <see cref="IMetricsOutputFormatter" /> used to write metrics.
+        /// </value>
+        public IMetricsOutputFormatter MetricsOutputFormatter { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the interval between flushing metrics.
+        /// </summary>
+        public TimeSpan FlushInterval { get; set; }
+    }
+}
