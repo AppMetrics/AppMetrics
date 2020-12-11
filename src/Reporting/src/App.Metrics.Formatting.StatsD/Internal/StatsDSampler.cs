6b6b1297115c27ef7b4e927c44a1f279ded40228
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-12-11 17:23:40 +0700

    Change how sample rate is implemented, use random sampler instead of deterministic sampler

--- src/Reporting/src/App.Metrics.Formatting.StatsD/Internal/StatsDSampler.cs
+++ src/Reporting/src/App.Metrics.Formatting.StatsD/Internal/StatsDSampler.cs
@@ -2,60 +2,21 @@
 // Copyright (c) App Metrics Contributors. All rights reserved.
 // </copyright>
 
-using System;
-
 namespace App.Metrics.Formatting.StatsD.Internal
 {
     internal class StatsDSampler
     {
-        private double _accumulator;
-        private long _lastTimestamp;
-        private double _sampleRate;
+        private readonly ThreadSafeRandom _random;
 
-        public StatsDSampler(double sampleRate)
+        public StatsDSampler()
         {
-            SampleRate = sampleRate;
-            _accumulator = 1.0;
-            _lastTimestamp = 0;
+            _random = new ThreadSafeRandom();
         }
 
-        public double SampleRate
+        public bool ShouldSend(double sampleRate)
         {
-            get => _sampleRate;
-            set
-            {
-                if (value < 0.0 || value > 1.0)
-                {
-                    throw new IndexOutOfRangeException("SampleRate must be in the range of 0.0 and 1.0 inclusive.");
-                }
-
-                _sampleRate = value;
-            }
-        }
-
-        public bool Sample()
-            => Sample(StatsDSyntax.FormatTimestamp(DateTime.UtcNow));
-
-        public bool Sample(long timeStamp)
-        {
-            if (_lastTimestamp == timeStamp)
-            {
-                return true;
-            }
-
-            _lastTimestamp = timeStamp;
-
-            var epsilon = 1.0 - double.Epsilon;
-            var result = _accumulator >= epsilon;
-
-            if (result)
-            {
-                _accumulator--;
-            }
-
-            _accumulator += SampleRate;
-
-            return result;
+            var d = _random.NextDouble;
+            return d < sampleRate;
         }
     }
 }
