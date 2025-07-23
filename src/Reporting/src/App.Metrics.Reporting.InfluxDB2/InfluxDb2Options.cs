6f66f68021eb96466ea672b6e02c625c949d7eb9
Allan Hardy <alhardy@users.noreply.github.com>
2025-07-23 21:53:54 +1000

    Dev (#777)
    * Added reporter for InfluxDB v2 (#701)
    
    Co-authored-by: Rok Bajec <rok.bajec@sowalabs.com>
    Co-authored-by: Allan Hardy <alhardy@users.noreply.github.com>
    
    * init 4.4.0 commit
    
    * Update azure-pipeline.yml for Azure Pipelines
    
    * Update azure-pipeline.yml for Azure Pipelines
    
    ---------
    
    Co-authored-by: rokbjc <54315899+rokbjc@users.noreply.github.com>
    Co-authored-by: Rok Bajec <rok.bajec@sowalabs.com>

---
+++ src/Reporting/src/App.Metrics.Reporting.InfluxDB2/InfluxDb2Options.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="InfluxDBOptions.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace App.Metrics.Reporting.InfluxDB2
+{
+    /// <summary>
+    ///     Provides programmatic configuration for InfluxDB in the App Metrics framework.
+    /// </summary>
+    public class InfluxDb2Options
+    {
+        public InfluxDb2Options() {}
+
+
+        /// <summary>
+        ///     Gets formatted endpoint for writes to InfluxDB
+        /// </summary>
+        /// <value>
+        ///     The InfluxDB endpoint for writes.
+        /// </value>
+        public string Endpoint
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Organization))
+                {
+                    return null;
+                }
+                if (string.IsNullOrWhiteSpace(Bucket))
+                {
+                    return null;
+                }
+
+                var endpoint = $"api/v2/write?org={Uri.EscapeDataString(Organization)}&bucket={Uri.EscapeDataString(Bucket)}";
+
+                if (!string.IsNullOrWhiteSpace(Precision))
+                {
+                    endpoint += $"&precision={Uri.EscapeDataString(Precision)}";
+                }
+
+                return endpoint;
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets the base URI of the InfluxDB API.
+        /// </summary>
+        /// <value>
+        ///     The base URI of the InfluxDB API where metrics are flushed.
+        /// </value>
+        public Uri BaseUri { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the InfluxDB organization name used to report metrics.
+        /// </summary>
+        /// <value>
+        ///     The InfluxDB organization name where metrics are flushed.
+        /// </value>
+        public string Organization { get; set; }
+        /// <summary>
+        ///     Gets or sets the InfluxDB bucket name used to report metrics.
+        /// </summary>
+        /// <value>
+        ///     The InfluxDB bucket name where metrics are flushed.
+        /// </value>
+        public string Bucket { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the precision for the unix timestamps within the body line-protocol.
+        /// </summary>
+        /// <value>
+        ///     "ms" "s" "us" "ns"
+        /// </value>
+        public string Precision { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the InfluxDB token.
+        /// </summary>
+        /// <value>
+        ///     The InfluxDB Token.
+        /// </value>
+        public string Token { get; set; }
+
+    }
+}
---
+++ src/Reporting/test/App.Metrics.Reporting.InfluxDB2.Facts/InfluxDBSettingsTests.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="InfluxDBSettingsTests.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
+// </copyright>
+
+using System;
+using App.Metrics.Reporting.InfluxDB2;
+using FluentAssertions;
+using Xunit;
+
+namespace App.Metrics.Reporting.InfluxDB.Facts
+{
+    // ReSharper disable InconsistentNaming
+    public class InfluxDb2SettingsTests
+    // ReSharper restore InconsistentNaming
+    {
+        [Fact]
+        public void Can_generate_influx_write_endpoint()
+        {
+            var settings = new InfluxDb2Options
+            {
+                Organization = "testorg",
+                Bucket = "testbucket",
+                BaseUri = new Uri("http://localhost"),
+                Precision = "s"
+            };
+
+            settings.Endpoint.Should().Be("api/v2/write?org=testorg&bucket=testbucket&precision=s");
+        }
+    }
+}
